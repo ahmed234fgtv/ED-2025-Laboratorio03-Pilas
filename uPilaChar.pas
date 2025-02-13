bb390c76a2d0501e42161e14b866f7a596d8c21e
ahmed234fgtv <ahmed.ehab0676@gmail.com>
2025-02-13 12:37:35 +0100

    Update uPilaChar.pas

--- uPilaChar.pas
+++ uPilaChar.pas
@@ -1,12 +1,60 @@
 unit uPilaChar;
 
-{ 1.1 
-    Genera una unidad pila cuyos nodos almacenen un único carácter.
-    Implementa las funciones básicas para la pila:
-        - Inicializar la pila.
-        - Verificar si la pila está vacía.
-        - Apilar un carácter.
-        - Desapilar un carácter.
-        - Obtener el carácter en la cima de la pila sin desapilarlo.
-}
+interface
+
+type
+	tNodo = record
+		info: char;
+		ant: ^tNodo;
+ 	end;
+
+ 	tPilachars= ^tNodo;
+
+procedure initialize(var p:tPilachars);
+procedure push(var p:tPilachars; x:char);
+procedure pop(var p:tPilachars);
+function peek(p:tPilachars):char;
+function isEmpty(p:tPilachars):boolean;
+
+implementation
+
+procedure initialize(var p:tPilachars);
+	begin
+		p := nil; { Inicializa el puntero cima de la pila a nil }
+	end;
+
+procedure push(var p:tPilachars; x:char);
+	var
+		nuevo: ^tnodo;
+	begin
+		new(nuevo); { Crea un nuevo nodo }
+		nuevo^.info := x; { Asigna el valor al nuevo nodo }
+		nuevo^.ant := p; { Enlaza el nuevo nodo con la anterior cima }
+		p := nuevo; { Actualiza la cima de la pila }
+	end;
+
+procedure pop(var p:tPilachars);
+	var
+		aux: ^tnodo;
+	begin
+		if not isEmpty(p) then
+		begin
+			aux := p; { Apunta con un puntero auxiliar al nodo en la cima (a eliminar) }
+			p := p^.ant; { Actualiza la cima de la pila }
+			dispose(aux); { Libera la memoria del elemento a eliminar }
+		end;
+	end;
+
+function peek(p:tPilachars):char;
+	begin
+		if not isEmpty(p) then
+		peek := p^.info; { Devuelve la parte información de la cima de la pila }
+	end;
+
+function isEmpty(p:tPilachars): boolean;
+	begin
+		isEmpty := p = nil; { Devuelve true si la cima es nil }
+	end;
+
+end.
 
